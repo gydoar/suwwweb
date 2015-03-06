a6265c4a3e689307611e7826391fb43f198e8792
Andres Vega <gydoar@hotmail.com>
2015-03-06 08:22:06 -0500

    solucionando problema de theme Options

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php get_header(); ?>
 
-<?php if(of_get_option( 'example_text_mini' )) { ?>
+
 	<h1><?php echo of_get_option( 'example_text_mini'); ?></h1>
-<?php } ?>
+
 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
